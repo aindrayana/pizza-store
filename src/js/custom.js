8ef49d1978ff3168396c3c8d11a3d0958cecfff6
Aji Indrayana <ajivancouver@gmail.com>
2016-03-10 01:00:20 -0800

    build static template - home and menu section

---
+++ src/js/custom.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Home image-background
+		/* ---------------------------------------------- */
+
+		$(".screen-height").height($(window).height());
+		$(window).resize(function(){
+			$(".screen-height").height($(window).height());
+		});
+
+
+	});
+
+});
